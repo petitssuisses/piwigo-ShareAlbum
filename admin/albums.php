848f2e77a21215c429d0714102c764a428b76be5
Arnaud <arnaud@petitssuisses.com>
2017-11-27 23:37:29 +0100

    Fixed Warning: [mysql error 1146] Table 'xxx.piwigo_sharealbum' doesn't exist #17

--- admin/albums.php
+++ admin/albums.php
@@ -5,14 +5,16 @@ defined('SHAREALBUM_PATH') or die('Hacking attempt!');
 // | Shared Albums tab                                                     |
 // +-----------------------------------------------------------------------+
 
+global $prefixeTable;
+
 // 
 $shared_albums_query = "SELECT s.id, s.cat as 'category', c.name as `album`, s.user_id, u.username as `user`, s.code as 'code', s.creation_date as `creation_date`, count(l.id) as `visits`, max(l.visit_d) as `last_visit`
-	FROM piwigo_sharealbum s
-	LEFT JOIN piwigo_sharealbum_log l
+	FROM ".SHAREALBUM_TABLE." s
+	LEFT JOIN ".SHAREALBUM_TABLE_LOG." l
 		ON s.cat = l.cat_id 
-	LEFT JOIN piwigo_categories c
+	LEFT JOIN ".$prefixeTable."categories c
 		ON c.id = s.cat
-	LEFT JOIN `piwigo_users` u
+	LEFT JOIN ".$prefixeTable."users u
 		ON u.id = s.user_id
 	GROUP BY s.id";
 $shared_albums = query2array($shared_albums_query);
