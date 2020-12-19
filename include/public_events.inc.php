fb47f61eb90f07675362d982ac53778769a07f6d
Arnaud <petitssuisses@users.noreply.github.com>
2020-12-20 00:03:50 +0100

    Fixed #56
    Fixed #56 Share option should be available only on private albums containing at least picture

--- include/public_events.inc.php
+++ include/public_events.inc.php
@@ -111,7 +111,20 @@ function sharealbum_loc_end_page()
 				
 		} else {
 			// No sharing detected
-			$template->assign('SHAREALBUM_LINK_CREATE',get_root_url()."?".SHAREALBUM_URL_ACTION."=".SHAREALBUM_URL_ACTION_CREATE."&".SHAREALBUM_URL_CATEGORY."=".$page['category']['id']);
+			
+			// Check if album can be shared (is private and contains at least 1 picture) - Implemented #56
+			$result_chk = pwg_query("SELECT COUNT(ic.image_id) as nb
+				FROM ".CATEGORIES_TABLE." c, ".IMAGE_CATEGORY_TABLE." ic 
+				WHERE c.id = ".$page['category']['id']." 
+				AND c.status = 'private'
+				AND c.id = ic.category_id");
+			if (pwg_db_num_rows($result_chk)) 
+			{
+				$row_chk = pwg_db_fetch_assoc($result_chk);
+				if ($row_chk['nb'] > 0) {
+					$template->assign('SHAREALBUM_LINK_CREATE',get_root_url()."?".SHAREALBUM_URL_ACTION."=".SHAREALBUM_URL_ACTION_CREATE."&".SHAREALBUM_URL_CATEGORY."=".$page['category']['id']);
+				}
+			}
 		}
 	}
 }
