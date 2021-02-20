616be1a47571ee8f1d60813034cd10280b1321c1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-20 21:05:48 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = 'Hierdoor wordt de verbinding met het huidige album verbroken en wordt de identificatiepagina geladen. Bevestig je dit?';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = 'Je gaat de gedeelde link voor dit album vernieuwen. De eerder gecommuniceerde link zal niet meer actief zijn. Bevestig je dit?';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = 'Als deze optie is aangevinkt, kunnen gebruikers teruggaan naar de root-Piwigo-url en automatisch inloggen om door het laatst bezochte gedeelde album te bladeren';
+$lang['When checked, menus are hidden for visitors of the shared album'] = 'Indien aangevinkt, zijn menu\'s verborgen voor bezoekers van het gedeelde album';
+$lang['When checked, breadcrumbs are replaced with the album name'] = 'Indien aangevinkt, worden broodkruimels vervangen door de albumnaam';
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = 'Indien aangevinkt, wordt een login-menu getoond voor gasten die browsen via een gedeelde link';
+$lang['Visit date'] = 'Bezoekdatum';
+$lang['This album is shared via a public link'] = 'Dit album wordt gedeeld via een openbare link';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = 'De link is niet naar het klembord gekopieerd. Mogelijk ondersteunt jouw browser deze functie nu.';
+$lang['Show links'] = 'Links weergeven';
+$lang['Show a login menu'] = 'Toon een login-menu';
+$lang['Shared link'] = 'Gedeelde link';
+$lang['Share this album'] = 'Deel dit album';
+$lang['Share link was successfully created. Click the share button to display it.'] = 'De link voor delen is succesvol gemaakt Klik op de deelknop om deze weer te geven.';
+$lang['Share a new album'] = 'Deel een nieuw album';
+$lang['Share'] = 'Deel';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = 'Stelt onthoud-mij-cookie in voor ingelogde gasten (auto-login)';
+$lang['Replace navigation breadcrumbs with album name'] = 'Vervang navigatiebroodkruimels door albumnaam';
+$lang['Renew link'] = 'Link vernieuwen';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = 'Selecteer de link en gebruik de functie Bewerken> Kopiëren in je browser';
+$lang['Please choose an action : '] = 'Kies een actie:';
+$lang['Number of displayed images per page for shared albums'] = 'Aantal weergegeven afbeeldingen per pagina voor gedeelde albums';
+$lang['No shared album'] = 'Geen gedeeld album';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Geen privéalbum om te delen (of dat nog niet is gedeeld)';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Link is succesvol vernieuwd. Klik op de deelknop om deze weer te geven.';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Link is succesvol verwijderd. Album wordt niet langer openbaar gedeeld.';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'De link is met succes naar het klembord gekopieerd. Je kunt nu de systeemplakfunctionaliteit gebruiken om het te delen!';
+$lang['Last visit'] = 'Laatste bezoek';
+$lang['IP address'] = 'IP-adres';
+$lang['Hide menus for albums visitors'] = 'Verberg menu\'s voor albumbezoekers';
+$lang['Descending'] = 'Aflopend';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Bepaalt het maximale aantal foto\'s dat per pagina wordt weergegeven voor gedeelde albums. Deze instelling is van toepassing op alle gedeelde albums';
+$lang['Ascending'] = 'Oplopend';
+$lang['Copy to clipboard'] = 'Kopieer naar klembord';
+$lang['Connection log for selected album'] = 'Verbindingslogboek voor geselecteerd album';
+$lang['Cancel sharing'] = 'Annuleer het delen';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Weet je zeker dat je het delen van dit album wilt annuleren?';
+$lang['Active shares'] = 'Gedeelde albums';
+$lang['Activity logs shown below'] = 'Activiteitenlogboeken hieronder weergegeven';
