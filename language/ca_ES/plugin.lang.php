09de68416e25f9c7deeae7872d0f7fbc2e6cfa0a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-21 21:25:08 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = 'Estableix la galeta (cookie) Recorda\'m per a usuaris amb sessió iniciada (auto-login)';
+$lang['Share'] = 'Comparteix';
+$lang['Share a new album'] = 'Comparteix un nou àlbum';
+$lang['Share link was successfully created. Click the share button to display it.'] = 'L\'enllaç de compartició s\'ha creat correctament. Feu clic al botó Comparteix per visualitzar-lo.';
+$lang['Share this album'] = 'Comparteix aquest àlbum';
+$lang['Shared link'] = 'Enllaç compartit';
+$lang['Show a login menu'] = 'Mostra un menú d\'inici de sessió';
+$lang['Show links'] = 'Mostra enllaços';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = 'L\'enllaç no s\'ha copiat al portapapers. Potser el vostre navegador no suporta aquesta funció.';
+$lang['This album is shared via a public link'] = 'Aquest àlbum s\'ha compartit a través d\'un enllaç públic';
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = 'Això us desconnectarà de l\'àlbum actual i carregarà la pàgina d\'identificació. Confirmeu l\'operació?';
+$lang['Visit date'] = 'Data de visita';
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = 'Quan està seleccionat, es mostra un menú d\'inici de sessió per als convidats que arribin mitjançant un enllaç compartit';
+$lang['When checked, breadcrumbs are replaced with the album name'] = 'Quan està seleccionat, el rastre de navegació se substitueix pel nom de l\'àlbum';
+$lang['When checked, menus are hidden for visitors of the shared album'] = 'Quan està seleccionat, s\'amaguen els menús per als visitants de l\'àlbum compartit';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = 'Quan està seleccionat, els usuaris poden tornar enrere fins a l\'URL d\'inici de Piwigo i iniciar sessió automàticament per visualitzar l\'últim àlbum compartit visitat';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = 'Esteu a punt de renovar l\'enllaç compartit d\'aquest àlbum. Qualsevol enllaç compartit anterior deixarà de funcionar. Confirmeu l\'operació?';
+$lang['No shared album'] = 'No hi ha cap àlbum compartit';
+$lang['Number of displayed images per page for shared albums'] = 'Nombre d\'imatges mostrades per pàgina en els àlbums compartits';
+$lang['Please choose an action : '] = 'Seleccioneu una acció:';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = 'Seleccioneu l\'enllaç i utilitzeu la funció Edita->Copia del navegador.';
+$lang['Renew link'] = 'Renova l\'enllaç';
+$lang['Replace navigation breadcrumbs with album name'] = 'Reemplaça el rastre de navegació pel nom de l\'àlbum';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'L\'enllaç s\'ha copiat correctament al portapapers. Ara podeu utilitzar la funció d\'enganxat del sistema per compartir-lo.';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'L\'enllaç s\'ha suprimit correctament. L\'àlbum ja no està compartit publicament.';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'L\'enllaç s\'ha renovat correctament. Feu clic al botó Comparteix per visualitzar-lo.';
+$lang['No private album to be shared (or which is not yet shared)'] = 'No hi ha cap àlbum privat que es pugui compartir (o que no s\'hagi compartit ja)';
+$lang['Active shares'] = 'Àlbums compartits';
+$lang['Cancel sharing'] = 'Cancel·la la compartició';
+$lang['Activity logs shown below'] = 'A continuació es mostren els registres d\'activitat';
+$lang['Ascending'] = 'Ascendent';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Segur que desitgeu cancel·lar aquesta compartició d\'àlbum?';
+$lang['Connection log for selected album'] = 'Registre de connexions per a l\'àlbum seleccionat';
+$lang['Copy to clipboard'] = 'Copia al portapapers';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Defineix el nombre màxim d\'imatges per pàgina per als àlbums compartits. Aquest valor s\'aplica a tots els àlbums compartits.';
+$lang['Descending'] = 'Descendent';
+$lang['Hide menus for albums visitors'] = 'Oculta els menús per a visitants dels àlbums';
+$lang['Last visit'] = 'Darrera visita';
+$lang['IP address'] = 'Adreça IP';
