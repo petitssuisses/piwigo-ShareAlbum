fe59d3f0f9696eb38996295e9d46e9653bbbf441
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-21 19:33:47 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Renew link'] = 'Renovar link';
+$lang['Replace navigation breadcrumbs with album name'] = 'Substitua o caminho de navegação pelo nome do álbum';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = 'Conjuntos lembrem-se de mim cookie para logado em convidados (login automático)';
+$lang['Share'] = 'Compartilhar';
+$lang['Share a new album'] = 'Compartilhar novo álbum';
+$lang['Share link was successfully created. Click the share button to display it.'] = 'O link de compartilhamento foi criado com sucesso. Clique no botão compartilhar para exibi-lo.';
+$lang['Share this album'] = 'Compartilhar este álbum';
+$lang['Shared link'] = 'Link compartilhado';
+$lang['Show a login menu'] = 'Mostre um menu de login';
+$lang['Show links'] = 'Mostrar links';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = 'O link não foi copiado para a área de transferência. Seu navegador pode agora suportar essa função.';
+$lang['This album is shared via a public link'] = 'Este álbum é compartilhado através de um link público';
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = 'Isso irá desconectá-lo do álbum atual e carregar a página de identificação. Você confirma?';
+$lang['Visit date'] = 'Data da visita';
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = 'Quando verificado, um menu de login é mostrado para os hóspedes que navegam através de um link compartilhado';
+$lang['When checked, breadcrumbs are replaced with the album name'] = 'Quando verificado, caminhos são substituídas pelo nome do álbum';
+$lang['When checked, menus are hidden for visitors of the shared album'] = 'Quando verificados, os menus são escondidos para os visitantes do álbum compartilhado';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = 'Quando verificado, os usuários podem voltar para root Piwigo url e automaticamente logado para navegar no último álbum compartilhado visitado';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = 'Você vai renovar o link compartilhado para este álbum. O link previamente comunicado não estará mais ativo. Você confirma?';
+$lang['Active shares'] = 'Álbuns compartilhados';
+$lang['Activity logs shown below'] = 'Registros de atividades mostrados abaixo';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Tem certeza que deseja cancelar o compartilhamento deste álbum?';
+$lang['Ascending'] = 'Crescente';
+$lang['Cancel sharing'] = 'Cancelar compartilhamento';
+$lang['Connection log for selected album'] = 'Registro de conexão para álbum selecionado';
+$lang['Copy to clipboard'] = 'Copiar para área de transferência';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Define o número de imagens máximas exibidas por página para álbuns compartilhados. Esta configuração se aplica a todos os álbuns compartilhados';
+$lang['Descending'] = 'Decrescente ';
+$lang['Hide menus for albums visitors'] = 'Ocultar menus para visitantes de álbuns';
+$lang['IP address'] = 'Endereços IP';
+$lang['Last visit'] = 'Última visita';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'Link foi copiado com sucesso para a área de transferência. Agora você pode usar a função de colar do sistema para compartilhá-lo !';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Link foi excluído com sucesso. O álbum não é mais compartilhado publicamente.';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Link foi renovado com sucesso. Clique no botão compartilhar para exibi-lo.';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Nenhum álbum privado a ser compartilhado (ou que ainda não é compartilhado)';
+$lang['No shared album'] = 'Nenhum álbum compartilhado';
+$lang['Number of displayed images per page for shared albums'] = 'Número de imagens exibidas por página para álbuns compartilhados';
+$lang['Please choose an action : '] = 'Por favor, escolha uma ação:';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = 'Selecione o link e use a função Editar>Copiar do seu navegador.';
