5d8159b48d335e523f65066c02a491a7e95feb51
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-22 21:49:07 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = 'Du er ved at forny dette albums delte link. Tidligere offentliggjorte links vil ikke længere fungere. Er du sikker?';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Definerer det maksimale antal billeder som vises pr. side i delte albummer. Denne indstilling gælder alle delte albummer.';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'Linket blev kopieret til klippebordet. Du kan nu anvende systemets indsætfunktion for at dele det!';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = 'Linket blev ikke kopieret til klippebordet. Måske er funktionen ikke understøttet i din browser.';
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = 'Dette vil fjerne din forbindelse til det aktuelle album og indlæse loginsiden. Er du sikker?';
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = 'Når afkrydset vil en loginmenu blive vist for gæster der browser gennem et delt link';
+$lang['When checked, breadcrumbs are replaced with the album name'] = 'Når afkrydset vil brødkrummer blive erstattet af albumnavne';
+$lang['When checked, menus are hidden for visitors of the shared album'] = 'Når afkrydset vil menuer blive skjult for besøgende i delte albummer';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = 'Når afkrydset kan brugerne gå tilbage til Piwigos rod-url, og automatisk bkive logget på for at kigge i det senest besøgte delte album';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Link blev slettet. Albummet deles ikke længere offentligt.';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Link blev fornyet. Klik på del-knappen for at vise det.';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Ingen private albummer at dele (eller som endnu ikke er delt)';
+$lang['Replace navigation breadcrumbs with album name'] = 'Erstat navigeringsbrødkrummer med albumnavne';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = 'Opsætter husk me-cookie for indloggede brugere (autologin)';
+$lang['Share link was successfully created. Click the share button to display it.'] = 'Delingslink blev oprettet. Klik på del-knappen for at vise det.';
+$lang['Renew link'] = 'Forny link';
+$lang['Share'] = 'Del';
+$lang['Share a new album'] = 'Del et nyt album';
+$lang['Share this album'] = 'Del dette album';
+$lang['Shared link'] = 'Delt link';
+$lang['Show a login menu'] = 'Vis en loginmenu';
+$lang['Show links'] = 'Vis links';
+$lang['This album is shared via a public link'] = 'Dette album deles gennem et offentligt link';
+$lang['Visit date'] = 'Besøgsdato';
+$lang['Active shares'] = 'Delte albummer';
+$lang['Activity logs shown below'] = 'Aktivitetslogninger kan ses nedenfor';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Er du sikker på at du vil annullere deling af dette album?';
+$lang['Ascending'] = 'Stigende';
+$lang['Cancel sharing'] = 'Annuller deling';
+$lang['Connection log for selected album'] = 'Forbindelseslog for valgt album';
+$lang['Copy to clipboard'] = 'Kopier til klippebord';
+$lang['Descending'] = 'Faldende';
+$lang['Hide menus for albums visitors'] = 'Skjul menuer for albumbesøgende';
+$lang['IP address'] = 'IP-adresse';
+$lang['Last visit'] = 'Seneste besøg';
+$lang['No shared album'] = 'Ingen delte albummer';
+$lang['Number of displayed images per page for shared albums'] = 'Antal viste billeder pr. side i delte albummer';
+$lang['Please choose an action : '] = 'Vælg en handling:';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = 'Vælg linket og benyt Rediger > Kopier-funktionen i din browser.';
