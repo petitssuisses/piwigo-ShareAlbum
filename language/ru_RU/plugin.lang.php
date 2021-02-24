4d8923b2948d9d4e1a1f8ccde2146086a2c04bb9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-24 12:07:22 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Share'] = 'Поделиться';
+$lang['Replace navigation breadcrumbs with album name'] = 'Заменить строку навигации ("хлебные крошки") именем альбома';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = 'Применять куки для входа (в т.ч. для автологина)';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Нет приватного альбома, который возможно сделать общим (или который еще не стал общим)';
+$lang['Share a new album'] = 'Поделиться новым альбомом';
+$lang['Share this album'] = 'Поделиться этим альбомом';
+$lang['Shared link'] = 'Общая ссылка';
+$lang['Show a login menu'] = 'Показать меню входа';
+$lang['Show links'] = 'Показать ссылки';
+$lang['Visit date'] = 'Дата визита';
+$lang['Active shares'] = 'Общие альбомы';
+$lang['Activity logs shown below'] = 'Журналы активности показаны ниже';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Вы уверены, что хотите отменить совместное использование этого альбома?';
+$lang['Cancel sharing'] = 'Отмена совместного использования';
+$lang['Connection log for selected album'] = 'Журнал подключений для выбранного альбома';
+$lang['Copy to clipboard'] = 'Копировать в буфер';
+$lang['Hide menus for albums visitors'] = 'Скрыть меню для альбомов гостей';
+$lang['IP address'] = 'IP адрес ';
+$lang['Last visit'] = 'Последний визит';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Ссылка успешна удалена. Альбом больше недоступен для публичного просмотра.';
+$lang['No shared album'] = 'Нет общего альбома';
+$lang['Number of displayed images per page for shared albums'] = 'Количество изображений на странице с общими альбомами';
+$lang['Please choose an action : '] = 'Пожалуйста, выберите действие:';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = 'Пожалуйста, выберите ссылку, используя фунции Вашего браузера (Редактировать > Копировать)';
+$lang['Renew link'] = 'Обновление ссылки';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Ссылка успешно обновлена. Нажмите кнопку «Поделиться», чтобы отобразить ее.';
+$lang['Ascending'] = 'По возрастанию';
+$lang['Descending'] = 'По убыванию';
