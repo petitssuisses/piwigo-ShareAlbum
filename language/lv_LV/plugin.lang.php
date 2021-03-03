6557a9c0b32bd52145348e1609f524e1fecc869a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-03-03 21:25:36 +0100

    Update lv_LV, thanks to agrisans

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['When checked, breadcrumbs are replaced with the album name'] = 'Kad pārbaudīts, pagaidu nosaukums tiek aizstāts ar albuma nosaukumu';
+$lang['This album is shared via a public link'] = 'Šis albums tiek kopīgots, izmantojot publisku saiti';
+$lang['Number of displayed images per page for shared albums'] = 'Parādīto attēlu skaits vienā lapā koplietotajiem albumiem';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Nav privāta albuma, ko kopīgot (vai kurš vēl nav kopīgots)';
+$lang['Show a login menu'] = 'Parādīt pieteikšanās izvēlni';
+$lang['Visit date'] = 'Apmeklējuma datums';
+$lang['Renew link'] = 'Atjaunināt saiti';
+$lang['Share'] = 'Koplietot';
+$lang['Share a new album'] = 'Koplietot jaunu albūmu';
+$lang['Share this album'] = 'Koplietot šo albūmu';
+$lang['Shared link'] = 'Koplietota saite';
+$lang['Show links'] = 'Rādīt saites';
+$lang['Please choose an action : '] = 'Lūdzu izvēlieties darbību';
+$lang['No shared album'] = 'Nav koplietota albūma';
+$lang['Descending'] = 'Dilstošā';
+$lang['Last visit'] = 'Pēdējā vizīte';
+$lang['IP address'] = 'IP adrese';
+$lang['Copy to clipboard'] = 'Kopēt starpliktuvē';
+$lang['Connection log for selected album'] = 'Savienojuma logs atlasītajam albumam';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Vai tiešām vēlaties atcelt šī albuma kopīgošanu?';
+$lang['Ascending'] = 'Augošā';
+$lang['Activity logs shown below'] = 'Darbību žurnāli, kas parādīti zemāk';
+$lang['Cancel sharing'] = 'Atcelt koplietošanu';
+$lang['Active shares'] = 'Koplietotie albumi';
