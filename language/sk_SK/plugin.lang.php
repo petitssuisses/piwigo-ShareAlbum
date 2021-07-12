4fd979e919db8a810af5810cb394ab0829e08dad
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-07-12 06:43:30 +0200

    Update sk_SK, thanks to jose1711

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Určuje maximálny počet zobrazených obrázkov na stránke zdieľaného albumu. Toto nastavenie platí pre všetky zdieľané albumy';
+$lang['Enable non-admin powerusers to share albums'] = 'Povoliť zdieľanie albumov neadministrátorom v skupine powerusers';
+$lang['When checked, menus are hidden for visitors of the shared album'] = 'Ak je zapnuté, ponuky sa nebudú návštevníkom zdieľaného albumu zobrazovať';
+$lang['When checked, nested albums are shared as well as the parent shared album'] = 'Zapnutím tejto voľby povolíte prístup aj do vnorených albumov';
+$lang['Activity logs shown below'] = 'Záznam aktivity je zobrazený nižšie';
+$lang['Connection log for selected album'] = 'Zaznamenané pripojenia vybraného albumu';
+$lang['Number of displayed images per page for shared albums'] = 'Počet zobrazených obrázkov na stránke zdieľaných albumov';
+$lang['Show a login menu'] = 'Zobraziť ponuku prihlásenia';
+$lang['Show links'] = 'Zobraziť odkazy';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = 'Odkaz nebol skopírovaný do schránky. Váš prehliadač zrejme nepodporuje túto funkciu.';
+$lang['This album is shared via a public link'] = 'Tento album je zdieľaný prostredníctvom verejného odkazu';
+$lang['Visit date'] = 'Dátum návštevy';
+$lang['Hide menus for albums visitors'] = 'Skryť ponuky pred návštevníkmi albumu';
+$lang['No shared album'] = 'Žiadny album nie je zdieľaný';
+$lang['Please choose an action : '] = 'Prosím, vyberte akciu:';
+$lang['Renew link'] = 'Obnoviť odkaz';
+$lang['Share'] = 'Zdieľať';
+$lang['Share a new album'] = 'Zdieľať nový album';
+$lang['Share link was successfully created. Click the share button to display it.'] = 'Odkaz na zdieľanie bol úspešne vytvorený. Kliknite na tlačidlo zdieľania pre jeho zobrazenie.';
+$lang['Share this album'] = 'Zdieľať tento album';
+$lang['Shared by'] = 'Zdieľaný';
+$lang['Shared link'] = 'Zdieľaný odkaz';
+$lang['Active shares'] = 'Zdieľané albumy';
+$lang['Advanced options'] = 'Pokročilé voľby';
+$lang['Apply shares to sub-albums'] = 'Použiť zdieľanie na podalbumy';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Naozaj chcete zrušiť zdieľanie tohto albumu?';
+$lang['Ascending'] = 'Vzostupne';
+$lang['Cancel sharing'] = 'Zrušiť zdieľanie';
+$lang['Copy to clipboard'] = 'Kopírovať do schránky';
+$lang['Descending'] = 'Zostupne';
+$lang['IP address'] = 'IP adresa';
+$lang['Last visit'] = 'Posledná návšteva';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'Odkaz bol úspešne skopírovaný do schránky. Vložením ho zdieľajte s inými používateľmi!';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Odkaz bol úspešne odstránený. Album už nie je verejne zdieľaný.';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Odkaz bol úspešne obnovený. Kliknite na tlačidlo zdieľania pre jeho zobrazenie.';
