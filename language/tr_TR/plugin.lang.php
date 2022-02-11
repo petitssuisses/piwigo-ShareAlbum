2311cad92a9ccf0266d95de7d5ee86eac9f17e4b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-02-11 06:35:57 +0100

    Update tr_TR, thanks to kapak

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['No shared album'] = 'Paylaşılan albüm yok ';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Paylaşılacak (veya henüz paylaşılmamış) özel albüm yok';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Bağlantı başarıyla yenilendi. Görüntülemek için paylaş düğmesini tıklayın.';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Bağlantı başarıyla silindi. Albüm artık herkese açık olarak paylaşılmıyor.';
+$lang['Last visit'] = 'Son ziyaret';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'Bağlantı başarıyla panoya kopyalandı. Artık paylaşmak için sistemin yapıştırma işlevini kullanabilirsiniz!';
+$lang['Enable non-admin powerusers to share albums'] = 'Yönetici olmayan ileri düzey kullanıcıların albümleri paylaşmasına izin verin';
+$lang['For this, add power users into the group <b>\'sharealbum_powerusers\'</b>.'] = 'Bunun için uzman kullanıcıları <b>\'sharealbum_powerusers\'</b> grubuna ekleyin.';
+$lang['Hide menus for albums visitors'] = 'Albüm ziyaretçileri için menüleri gizle';
+$lang['IP address'] = 'IP adresi';
+$lang['Descending'] = 'Azalan';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Paylaşılan albümler için sayfa başına görüntülenecek maksimum resim sayısını tanımlar. Bu ayar tüm paylaşılan albümler için geçerlidir';
+$lang['Copy to clipboard'] = 'Panoya kopyala';
+$lang['Connection log for selected album'] = 'Seçilen albüm için bağlantı günlüğü';
+$lang['Cancel sharing'] = 'Paylaşımı iptal et';
+$lang['Ascending'] = 'Artan';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Bu albüm paylaşımını iptal etmek istediğinizden emin misiniz? ';
+$lang['Apply shares to sub-albums'] = 'Paylaşımları alt albümlere uygula ';
+$lang['Advanced options'] = 'Gelişmiş seçenekler';
+$lang['Activity logs shown below'] = 'Aşağıda gösterilen etkinlik günlükleri ';
+$lang['Active shares'] = 'Albüm Paylaş';
