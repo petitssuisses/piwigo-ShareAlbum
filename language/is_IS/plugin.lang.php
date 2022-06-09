14471c498ec7dfadff0e4766fa5887865c36943c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-09 06:31:47 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = 'Þegar hakað er við þá birtist innskráningarvalmynd fyrir gesti sem vafra með deilitengli';
+$lang['When checked, any user belonging to the group \'sharealbum_powerusers\' has access to the share option in private album pages'] = 'Þegar hakað er við hafa allir notendur sem tilheyra hópnum \'sharealbum_powerusers\' aðgang að deilingarvalkostinum á einkaalbúmsíðum';
+$lang['When checked, breadcrumbs are replaced with the album name'] = 'Þegar hakað er við er brauðmylsnu skipt út fyrir albúmsheiti';
+$lang['When checked, menus are hidden for visitors of the shared album'] = 'Þegar hakað er við þá eru valmyndir faldar fyrir gesti deilanlegra albúma';
+$lang['When checked, nested albums are shared as well as the parent shared album'] = 'Þegar hakað er við, er einnig hægt að fletta í undiralbúmum';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = 'Þegar hakað er við geta notendur farið aftur á rótarslóð Piwigo, eru sjálfkrafa skráðir inn til að skoða síðast heimsótta deilda albúmið';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = 'Þú ætlar að endurnýja deilitengilinn fyrir þetta albúm. Tengill sem áður var tilkynntur verður ekki lengur virkur. Ertu viss?';
+$lang['No shared album'] = 'Ekkert deilt albúm';
+$lang['Number of displayed images per page for shared albums'] = 'Fjöldi birtra mynda á hverri síðu fyrir deild albúm';
+$lang['Please choose an action : '] = 'Vinsamlega veldu aðgerð:';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = 'Vinsamlega veldu tengilinn og notaðu Breyta > Afrita aðgerðina í vafranum þínum.';
+$lang['Renew link'] = 'Endurnýja tengil';
+$lang['Replace navigation breadcrumbs with album name'] = 'Skiptu út leiðsögubrauðmolum fyrir albúmheiti';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = 'Setur muna mig fótspor fyrir innskráða gesti (sjálfvirk innskráning)';
+$lang['Share'] = 'Deila';
+$lang['Share a new album'] = 'Deila nýju albúmi';
+$lang['Share link was successfully created. Click the share button to display it.'] = 'Deilitengill var útbúinn. Smelltu á deilingarhnappinn til að birta hann.';
+$lang['Share this album'] = 'Deila þessu albúmi';
+$lang['Shared by'] = 'Deilt af';
+$lang['Shared link'] = 'Deilitengill';
+$lang['Show a login menu'] = 'Sýna innskráningarvalmynd';
+$lang['Show links'] = 'Sýna tengla';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = 'Tengillinn var ekki afritaður á klippiborðið. Vafrinn þinn styður kannski ekki þessa virkni.';
+$lang['This album is shared via a public link'] = 'Þessu albúmi er deilt með opinberum tengli';
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = 'Þetta mun aftengja þig frá núverandi albúmi og hlaða innskráningarsíðunni. Ertu viss?';
+$lang['Visit date'] = 'Heimsóknardagur';
+$lang['Warning : those users will be able to create shared albums as well as to remove existing shares'] = 'Viðvörun: þessir notendur munu geta búið til deilanleg albúm sem og að fjarlægja núverandi deilingar';
+$lang['Active shares'] = 'Deild albúm';
+$lang['Activity logs shown below'] = 'Athafnaskrár sýndar hér að neðan';
+$lang['Advanced options'] = 'Ítarlegir valkostir';
+$lang['Apply shares to sub-albums'] = 'Notaðu deilingar á undiralbúm';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Ertu viss um að þú viljir hætta við deilingu á þessu albúmi?';
+$lang['Ascending'] = 'Hækkandi';
+$lang['Cancel sharing'] = 'Hætta við deilingu';
+$lang['Connection log for selected album'] = 'Tengingarskrá fyrir valið albúm';
+$lang['Copy to clipboard'] = 'Afrita á klippiborð';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Skilgreinir hámarksfjölda mynda á hverri síðu fyrir deild albúm. Þessi stilling á við um öll deild albúm';
+$lang['Descending'] = 'Lækkandi';
+$lang['Enable non-admin powerusers to share albums'] = 'Gerðu stórnotendum sem ekki eru stjórnendur kleift að deila albúmum';
+$lang['For this, add power users into the group <b>\'sharealbum_powerusers\'</b>.'] = 'Til að gera þetta skaltu bæta stórnotendum við hópinn <b>\'sharealbum_powerusers\'</b>.';
+$lang['Hide menus for albums visitors'] = 'Fela valmyndir fyrir gesti albúma';
+$lang['IP address'] = 'IP tala';
+$lang['Last visit'] = 'Síðasta heimsókn';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'Tengill var afritaður á klippiborðið. Þú getur núna notað límvirkni kerfisins til að deila því!';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Tengli var eytt. Albúmi er ekki lengur deilt opinberlega.';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Tengill var endurnýjaður. Smelltu á deilingarhnappinn til að birta hann.';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Engu einkaalbúmi til að deila (eða sem er ekki enn deilt)';
