bbda2e2cdcc490e21d26716a1ca7bbf5bce49039
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-25 06:38:29 +0200

    Update cs_CZ, thanks to MiPi

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = 'Pokud je zaškrtnuto, zobrazí se hostům, kteří přicházejí přes sdílený odkaz, přihlašovací nabídka.';
+$lang['When checked, any user belonging to the group \'sharealbum_powerusers\' has access to the share option in private album pages'] = 'Pokud je zaškrtnuto, má každý uživatel patřící do skupiny \'sharealbum_powerusers\' na stránkách soukromých alb přístup k možnosti sdílení';
+$lang['When checked, breadcrumbs are replaced with the album name'] = 'Pokud je zaškrtnuto, nahradí drobečkovou navigaci jménem alba';
+$lang['When checked, menus are hidden for visitors of the shared album'] = 'Pokud je zaškrtnuto, jsou pro návštěvníky sdíleného alba skrytá menu';
+$lang['When checked, nested albums are shared as well as the parent shared album'] = 'Pokud je zaškrtnuto, lze procházet i vnořená alba';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = 'Pokud je zaškrtnuto, mohou se uživatelé vrátit na kořenové URL Piwigo, automaticky přihlášení k procházení naposledy navštíveného sdíleného alba';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = 'Chystáte se obnovit sdílený odkaz na toto album. Dříve sdílený odkaz již nebude aktivní. Jste si jisti?';
+$lang['Warning : those users will be able to create shared albums as well as to remove existing shares'] = 'Upozornění: tito uživatelé budou moci vytvářet sdílená alba i odstraňovat stávající sdílení';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = 'Odkaz nebyl zkopírován do schránky. Váš prohlížeč nemusí tuto funkci podporovat.';
+$lang['This album is shared via a public link'] = 'Toto album je sdíleno přes veřejný odkaz';
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = 'Tím se odpojíte od aktuálního alba a načte se přihlašovací stránka. Jste si jisti?';
+$lang['Visit date'] = 'Datum návštěvy';
+$lang['Shared link'] = 'Sdílený odkaz';
+$lang['Shared by'] = 'Sdílel';
+$lang['Show a login menu'] = 'Zobrazit přihlašovací menu';
+$lang['Show links'] = 'Zobrazit odkazy';
+$lang['No shared album'] = 'Žádné sdílené album';
+$lang['Number of displayed images per page for shared albums'] = 'Počet obrázků, které se mají zobrazit na jedné stránce sdílených alb';
+$lang['Please choose an action : '] = 'Vyberte prosím akci:';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = 'Označte odkaz a použijte funkci Úpravy > Kopírovat v prohlížeči.';
+$lang['Renew link'] = 'Obnovit odkaz';
+$lang['Replace navigation breadcrumbs with album name'] = 'Nahradit drobečkovou navigaci jménem alba';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = 'Nastaví cookie pro zapamatování přihlášeného hosta (automatické přihlášení)';
+$lang['Share'] = 'Sdílet';
+$lang['Share a new album'] = 'Sdílet nové album';
+$lang['Share link was successfully created. Click the share button to display it.'] = 'Odkaz na sdílení byl úspěšně vytvořen. Kliknutím na tlačítko sdílení jej zobrazíte.';
+$lang['Share this album'] = 'Sdílet toto album';
+$lang['For this, add power users into the group <b>\'sharealbum_powerusers\'</b>.'] = 'Za tímto účelem přidejte uživatele do skupiny <b>\'sharealbum_powerusers\'</b>.';
+$lang['Hide menus for albums visitors'] = 'Skrýt menu návštěvníkům alba';
+$lang['IP address'] = 'IP adresa';
+$lang['Last visit'] = 'Poslední návštěva';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'Odkaz byl úspěšně zkopírován do schránky. Nyní jej můžete sdílet pomocí systémové funkce \'Vložit\'!';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Odkaz byl úspěšně odstraněn. Album již není veřejně sdíleno.';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Odkaz byl úspěšně obnoven. Kliknutím na tlačítko sdílení ho zobrazíte.';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Žádné soukromé album ke sdílení (nebo ještě není sdílené)';
+$lang['Active shares'] = 'Sdílená alba';
+$lang['Activity logs shown below'] = 'Protokoly o činnosti uvedené níže';
+$lang['Advanced options'] = 'Rozšířené možnosti';
+$lang['Apply shares to sub-albums'] = 'Použít sdílení na podalba';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Opravdu chcete zrušit sdílení tohoto alba?';
+$lang['Ascending'] = 'Vzestupně';
+$lang['Cancel sharing'] = 'Zrušit sdílení';
+$lang['Connection log for selected album'] = 'Protokol o připojeních pro vybrané album';
+$lang['Copy to clipboard'] = 'Zkopírovat do schránky';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Definuje maximální počet obrázků, které se mají zobrazit na jedné stránce sdílených alb. Toto nastavení platí pro všechna sdílená alba';
+$lang['Descending'] = 'Sestupně';
+$lang['Enable non-admin powerusers to share albums'] = 'Umožnit sdílení alb uživatelům, kteří nejsou administrátory';
