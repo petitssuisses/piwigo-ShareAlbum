360d186d05d039a6f0141d220521f901c177ea5c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-07 06:27:08 +0100

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = 'När det är markerat visas en inloggningsmeny för gäster som surfar via en delad länk';
+$lang['When checked, any user belonging to the group \'sharealbum_powerusers\' has access to the share option in private album pages'] = 'När det är markerat har alla användare som tillhör gruppen \'sharealbum_powerusers\' åtkomst till delningsalternativet på privata albumsidor';
+$lang['When checked, breadcrumbs are replaced with the album name'] = 'När det är markerat ersätts brödsmulor med albumets namn';
+$lang['When checked, menus are hidden for visitors of the shared album'] = 'När det är markerat göms menyer för besökare till det delade albumet';
+$lang['When checked, nested albums are shared as well as the parent shared album'] = 'När det är markerat kan nästlade album också bläddras';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = 'När det är markerat kan användare gå tillbaka till Piwigos rot-URL, automatiskt inloggad för att bläddra i det senast besökta delade albumet';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = 'Du kommer att förnya den delade länken för detta album. Tidigare kommunicerad länk kommer inte längre att vara aktiv. Är du säker?';
+$lang['No shared album'] = 'Inget delat album';
+$lang['Number of displayed images per page for shared albums'] = 'Antal bilder per sida för delade album';
+$lang['Please choose an action : '] = 'Välj en åtgärd:';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = 'Var god välj länken och använd din webbläsares Redigera > Kopiera funktion.';
+$lang['Renew link'] = 'Förnya länk';
+$lang['Replace navigation breadcrumbs with album name'] = 'Ersätt navigeringsbrödsmulor med albumnamn';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = 'Sätter komihåg mig cookie för inloggade gäster (auto-inloggning)';
+$lang['Share'] = 'Dela';
+$lang['Share a new album'] = 'Dela ett nytt album';
+$lang['Share link was successfully created. Click the share button to display it.'] = 'Delningslänk har skapats. Klicka på dela-knappen för att visa den.';
+$lang['Share this album'] = 'Dela detta album';
+$lang['Shared by'] = 'Delat av';
+$lang['Shared link'] = 'Delad länk';
+$lang['Show a login menu'] = 'Visa en inloggningsmeny';
+$lang['Show links'] = 'Visa länkar';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = 'Länken kopierades inte till urklipp. Din webbläsare kanske inte stöder denna funktion.';
+$lang['This album is shared via a public link'] = 'Detta album delas via en offentlig länk';
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = 'Detta kommer att koppla bort dig från det aktuella albumet och ladda inloggningssidan. Är du säker?';
+$lang['Visit date'] = 'Besöksdatum';
+$lang['Warning : those users will be able to create shared albums as well as to remove existing shares'] = 'Varning: dessa användare kommer att kunna skapa delade album samt ta bort befintliga delningar';
+$lang['Active shares'] = 'Delade album';
+$lang['Activity logs shown below'] = 'Aktivitetsloggar visas nedan';
+$lang['Advanced options'] = 'Avancerade alternativ';
+$lang['Apply shares to sub-albums'] = 'Tillämpa delningar till underalbum';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Är du säker att du vill avsluta delningen av detta album?';
+$lang['Ascending'] = 'Stigande';
+$lang['Cancel sharing'] = 'Avsluta delning';
+$lang['Connection log for selected album'] = 'Anslutningslogg för valt album';
+$lang['Copy to clipboard'] = 'Kopiera till urklipp';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Definierar det maximala antalet bilder som ska visas per sida för delade album. Den här inställningen gäller alla delade album';
+$lang['Descending'] = 'Fallande';
+$lang['Enable non-admin powerusers to share albums'] = 'Tillåt avancerade användare som inte är administratörer att dela album';
+$lang['For this, add power users into the group <b>\'sharealbum_powerusers\'</b>.'] = 'För detta lägger du till avancerade användare i gruppen <b>\'sharealbum_powerusers\'</b>.';
+$lang['Hide menus for albums visitors'] = 'Göm menyer för albumbesökare';
+$lang['IP address'] = 'IP adress';
+$lang['Last visit'] = 'Senaste besök';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'Länken har kopierats till urklipp. Du kan nu använda systemets inklistringsfunktion för att dela det!';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Länken har raderats. Albumet delas inte längre offentligt.';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Länken har förnyats. Klicka på dela-knappen för att visa den.';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Inget privat album att dela (eller som ej ännu är delat)';
