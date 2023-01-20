cf5f43f3e154e532542bdbdb45b051974e66876e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-20 06:16:12 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = 'Quando spuntato, un menù di autenticazione viene mostrato per gli ospiti che navigano attraverso un collegamento condiviso.';
+$lang['When checked, any user belonging to the group \'sharealbum_powerusers\' has access to the share option in private album pages'] = 'Quando spuntato, qualunque utente del gruppo \'sharealbum_powerusers\' ha accesso alle opzioni di condivisione delle pagine dell\'album privato.';
+$lang['When checked, breadcrumbs are replaced with the album name'] = 'Quando spuntato, le briciole di pane vengono sostituite col nome dell\'album.';
+$lang['When checked, menus are hidden for visitors of the shared album'] = 'Quando spuntato, i menù vengono nascosti ai visitatori dell\'album condiviso.';
+$lang['When checked, nested albums are shared as well as the parent shared album'] = 'Quando spuntato, è possibile navigare anche negli album nidificati.';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = 'Quando spuntato, gli utenti possono tornare alla cartella di base di Piwigo, e saranno loggati automaticamente per sfogliare l\'ultimo album condiviso visitato.';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = 'Stai per aggiornare il collegamento condiviso per quest\'album. Il collegamento precedentemente comunicato non sarà più in funzione. Sei sicuro?';
+$lang['No shared album'] = 'Nessun album condiviso.';
+$lang['Number of displayed images per page for shared albums'] = 'Numero di immagini per pagina per album condiviso da mostrare.';
+$lang['Please choose an action : '] = 'Prego selezionare un\'azione.';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = 'Prego selezionare un collegamento ed utilizza la funzione Modifica > Copia del tuo browser.';
+$lang['Renew link'] = 'Aggiorna collegamento.';
+$lang['Replace navigation breadcrumbs with album name'] = 'Sostituisci le briciole di pane della navigazione con il nome dell\'album.';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = 'Imposta il cookie "ricordami" per gli ospiti che hanno effettuato l\'accesso (auto-login).';
+$lang['Share'] = 'Condividi.';
+$lang['Share a new album'] = 'Condividi un nuovo album.';
+$lang['Share link was successfully created. Click the share button to display it.'] = 'Il collegamento per la condivisione è stato creato con successo. Clicca sul pulsante di condivisione per visualizzarlo.';
+$lang['Share this album'] = 'Condividi questo album.';
+$lang['Shared by'] = 'Condiviso da';
+$lang['Shared link'] = 'Collegamento condiviso.';
+$lang['Show a login menu'] = 'Mostra un menù per l\'autenticazione.';
+$lang['Show links'] = 'Mostra i collegamenti.';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = 'Il collegamento non è stato copiato negli appunti. Forse il tuo browser non supporta tale funzionalità.';
+$lang['This album is shared via a public link'] = 'Questo album viene condiviso attraverso un collegamento pubblico.';
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = 'Questo ti scollegherà dall\'album corrente e caricherà la pagina di autenticazione. Sei sicuro? ';
+$lang['Visit date'] = 'Data delle visita.';
+$lang['Warning : those users will be able to create shared albums as well as to remove existing shares'] = 'Attenzione: questi utenti saranno in grado di creare album condivisi e rimuovere le condivisioni esistenti.';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Sei sicuro di voler cancellare questa condivisione dell\'album?';
+$lang['Ascending'] = 'Ascendente.';
+$lang['Cancel sharing'] = 'Cancella condivisione.';
+$lang['Connection log for selected album'] = 'Log di connessione per l\'album selezionato.';
+$lang['Copy to clipboard'] = 'Copia negli appunti.';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Definisce il massimo numero di immagini per pagina da mostrare per gli album condivisi.';
+$lang['Descending'] = 'Discendente.';
+$lang['Enable non-admin powerusers to share albums'] = 'Abilita utenti privilegiati non amministrativi per condividere gli album.';
+$lang['Hide menus for albums visitors'] = 'Nascondi menù per i visitatori dell\'album.';
+$lang['IP address'] = 'Indirizzo IP.';
+$lang['Last visit'] = 'Ultima visita.';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'Il collegamento è stato copiato con successo negli appunti. Ora puoi utilizzare la funzione di sistema incolla per condividerlo.';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Il collegamento è stato rimosso con successo. L\'album non è più condiviso pubblicamente.';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Il collegamento è stato aggiornato con successo. Clicca il pulsante di condivisione per visualizzarlo.';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Nessun album privato da condividere (o che non sia già stato condiviso).';
+$lang['For this, add power users into the group <b>\'sharealbum_powerusers\'</b>.'] = 'Per questo, aggiungi utenti privilegiati nel gruppo <b>\'sharealbum_powerusers\'</b>.';
+$lang['Activity logs shown below'] = 'Log dell\'attività mostrati sotto.';
+$lang['Apply shares to sub-albums'] = 'Applica condivisioni ai sotto album.';
+$lang['Advanced options'] = 'Opzioni avanzate.';
+$lang['Active shares'] = 'Album condivisi.';
