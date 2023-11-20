ff62e4e74db7f651e104b8438437211bbcb09f1d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-11-20 06:58:21 +0100

    Update el_GR, thanks to bas_alba

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['No shared album'] = 'Δεν υπάρχει κοινόχρηστο λεύκωμα.';
+$lang['Number of displayed images per page for shared albums'] = 'Αριθμός εμφανιζόμενων εικόνων ανά σελίδα για κοινόχρηστα λευκώματα';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Δεν υπάρχει ιδιωτικό λεύκωμα προς κοινή χρήση (ή που δεν είναι ακόμη κοινόχρηστο)';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Ο σύνδεσμος ανανεώθηκε με επιτυχία. Κάντε κλικ στο κουμπί κοινής χρήσης για να το εμφανίσετε.';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Ο σύνδεσμος διαγράφηκε με επιτυχία. Το λεύκωμα δεν κοινοποιείται πλέον δημόσια.';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'Ο σύνδεσμος αντιγράφηκε με επιτυχία στο πρόχειρο. Τώρα μπορείτε να χρησιμοποιήσετε τη λειτουργία επικόλλησης του συστήματος για να το μοιραστείτε!';
+$lang['Last visit'] = 'Τελευταία επίσκεψη';
+$lang['IP address'] = 'διεύθυνση IP';
+$lang['Hide menus for albums visitors'] = 'Απόκρυψη μενού για επισκέπτες λευκώματος';
+$lang['For this, add power users into the group <b>\'sharealbum_powerusers\'</b>.'] = 'Για αυτό, προσθέστε ισχυρούς χρήστες στην ομάδα';
+$lang['Enable non-admin powerusers to share albums'] = 'Ενεργοποιήστε την κοινή χρήση λευκωμάτων από χρήστες που δεν είναι διαχειριστές';
+$lang['Descending'] = 'Φθίνουσα';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Καθορίζει τον μέγιστο αριθμό εικόνων που θα εμφανίζονται ανά σελίδα για κοινόχρηστα Λευκώματα. Αυτή η ρύθμιση ισχύει για όλα τα κοινόχρηστα Λευκώματα';
+$lang['Copy to clipboard'] = 'Αντιγραφή στο πρόχειρο';
+$lang['Connection log for selected album'] = 'Αρχείο καταγραφής σύνδεσης για επιλεγμένο Λευκώματα';
+$lang['Cancel sharing'] = 'Ακύρωση κοινής χρήσης';
+$lang['Ascending'] = 'Αύξουσα';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Είστε βέβαιοι ότι θέλετε να ακυρώσετε την κοινή χρήση αυτού του Λευκώματος;';
+$lang['Apply shares to sub-albums'] = 'Εφαρμογή κοινών χρήσεων σε υπο-Λευκώματα';
+$lang['Advanced options'] = 'Προχωρημένες επιλογές';
+$lang['Activity logs shown below'] = 'Τα αρχεία καταγραφής δραστηριοτήτων φαίνονται παρακάτω';
+$lang['Active shares'] = 'Κοινόχρηστα Λευκώματα';
