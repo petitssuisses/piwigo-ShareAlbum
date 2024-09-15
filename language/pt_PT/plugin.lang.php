edf87aabf74581f817b8455f4cc5a9dd6f490b57
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-15 20:13:28 +0200

    Update pt_PT, thanks to ANO

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = 'Quando marcado, os utilizadores podem voltar para a URL raiz do Piwigo, automaticamente ligados para navegar o último album partilhado visitado';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = 'Você vai renovar o link partilhado para este album. O link anteriormente  comunicado não estará mais ativo. Tem a certeza?';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'O link foi copiado com êxito para a área de transferência. Agora você pode usar a funcionalidade de colagem para compartilhá-lo!';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'Link foi excluído com êxito. O album não é mais compartilhado publicamente.';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'Link foi renovado com sucesso. Clique no botão de partilhamento para exibi-lo.';
+$lang['No private album to be shared (or which is not yet shared)'] = 'Nenhum album privado a ser partilhado (ou ainda não foi partilhado)';
+$lang['Number of displayed images per page for shared albums'] = 'Número de imagens exibidas por página para albuns partilhados';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = 'Selecione o link e use a função Editar do navegador > Função copiar ';
+$lang['Replace navigation breadcrumbs with album name'] = 'Substitua na linha de navegação pelo nome do album';
+$lang['No shared album'] = 'Album não partilhado';
+$lang['Renew link'] = 'Renovar link';
+$lang['Share a new album'] = 'Partilhar um novo album';
+$lang['Shared by'] = 'Partilhado por';
+$lang['Apply shares to sub-albums'] = 'Aplicar partilha aos sub-albuns';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'Define o número máximo numero de imagens a mostrar por página dos albuns partilhados. Esta definição aplica-se a todos os albuns.';
+$lang['Active shares'] = 'Albuns partilhados.';
+$lang['Activity logs shown below'] = 'Registos de atividades mostrados abaixo';
+$lang['Advanced options'] = 'Opções avançadas';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'Tem a certeza de que pretende cancelar esta partilha de albuns?';
+$lang['Ascending'] = 'Ascendente';
+$lang['Cancel sharing'] = 'Cancelar partilha';
+$lang['Descending'] = 'Descendente';
+$lang['IP address'] = 'Direção de IP';
+$lang['Last visit'] = 'Última visita';
+$lang['Please choose an action : '] = 'Escolha uma ação';
+$lang['Share'] = 'Partilhar';
+$lang['Share this album'] = 'Partilhar este album';
+$lang['Shared link'] = 'Link de partilha';
+$lang['Show links'] = 'Mostrar links';
+$lang['Visit date'] = 'Data da visita';
