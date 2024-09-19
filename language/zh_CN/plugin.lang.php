590f94420cb54001a739bb4526a54ef12c53837d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-19 06:18:03 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Connection log for selected album'] = '选定相册的连接日志';
+$lang['No private album to be shared (or which is not yet shared)'] = '没有可供分享的私人相册（或尚未分享的私人相册）';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = '请选择链接并使用浏览器的编辑>复制功能。';
+$lang['Share'] = '分享';
+$lang['Share link was successfully created. Click the share button to display it.'] = '分享链接已成功创建。点击分享按钮即可显示该链接。';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = '链接未复制到剪贴板。您的浏览器可能不支持此功能。';
+$lang['This album is shared via a public link'] = '此相册通过公开链接分享';
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = '这将断开您与当前相册的连接并加载登录页面。您确定吗？';
+$lang['Visit date'] = '访问日期';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = '您将更新此相册的共享链接。之前传达的链接将不再有效。您确定吗？';
+$lang['Activity logs shown below'] = '活动日志显示如下';
+$lang['Are you sure you wish to cancel this album sharing ?'] = '您确定要取消此相册共享吗？';
+$lang['Advanced options'] = '高级选项';
+$lang['IP address'] = 'IP 地址';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = '链接已成功复制到剪贴板。您现在可以使用系统的粘贴功能来分享它！';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = '链接已成功删除。相册不再公开分享。';
+$lang['Link was successfully renewed. Click the share button to display it.'] = '链接已成功更新。点击分享按钮即可显示。';
+$lang['Copy to clipboard'] = '复制到剪贴板';
+$lang['Cancel sharing'] = '取消共享';
+$lang['Renew link'] = '更新链接';
+$lang['Shared by'] = '共享者';
+$lang['Shared link'] = '共享链接';
+$lang['Warning : those users will be able to create shared albums as well as to remove existing shares'] = '警告：这些用户将能够创建共享相册以及删除现有的共享';
+$lang['For this, add power users into the group <b>\'sharealbum_powerusers\'</b>.'] = '为此，将高级用户添加到组 <b>\\'sharealbum_powerusers\\'</b>.';
+$lang['When checked, any user belonging to the group \'sharealbum_powerusers\' has access to the share option in private album pages'] = '选中后，属于“sharealbum_powerusers”组的任何用户都可以访问私人相册页面中的共享选项';
+$lang['Enable non-admin powerusers to share albums'] = '允许非管理员用户共享相册';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = '定义共享相册每页显示的最大图片数量。此设置适用于所有共享相册';
+$lang['Number of displayed images per page for shared albums'] = '共享相册每页显示的图片数量';
+$lang['When checked, nested albums are shared as well as the parent shared album'] = '选中后，嵌套相册也可以浏览';
+$lang['Apply shares to sub-albums'] = '将共享应用于子相册';
+$lang['When checked, breadcrumbs are replaced with the album name'] = '选中后，面包屑被替换为相册名称';
+$lang['Replace navigation breadcrumbs with album name'] = '用相册名称替换导航面包屑';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = '选中后，用户可以返回Piwigo根URL，自动登录浏览上次访问的共享相册';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = '为已登录的访客设置记住我的 cookie（自动登录）';
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = '选中后，将为通过共享链接浏览的访客显示登录菜单';
+$lang['Show a login menu'] = '显示登录菜单';
+$lang['When checked, menus are hidden for visitors of the shared album'] = '选中后，共享相册的访问者将隐藏菜单';
+$lang['Hide menus for albums visitors'] = '向相册访问者隐藏菜单';
+$lang['Last visit'] = '上次访问';
+$lang['Ascending'] = '升序';
+$lang['Descending'] = '降序';
+$lang['Show links'] = '显示链接';
+$lang['No shared album'] = '没有共享相册';
+$lang['Please choose an action : '] = '请选择一个操作:';
+$lang['Share this album'] = '共享此相册';
+$lang['Share a new album'] = '共享新相册';
+$lang['Active shares'] = '共享相册';
