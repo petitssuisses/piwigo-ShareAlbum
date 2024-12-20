ca6de429d0b5b433f41cca9dc1a435bf8a36e4de
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:03:25 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Active shares'] = 'אלבומים משותפים';
+$lang['Activity logs shown below'] = 'יומני פעילות מוצגים למטה';
+$lang['Advanced options'] = 'אפשרויות מתקדמות';
+$lang['Apply shares to sub-albums'] = 'החל חלוקה לתת-אלבומים';
+$lang['Are you sure you wish to cancel this album sharing ?'] = 'האם אתה בטוח שברצונך לבטל את שיתוף האלבום הזה?';
+$lang['Ascending'] = 'עולה';
+$lang['Cancel sharing'] = 'ביטול שיתוף';
+$lang['Connection log for selected album'] = 'יומן חיבור לאלבום שנבחר';
+$lang['Copy to clipboard'] = 'העתק ללוח';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = 'מגדיר את מספר התמונות המרבי להצגה בכל עמוד עבור אלבומים משותפים. הגדרה זו חלה על כל האלבומים המשותפים';
+$lang['Descending'] = 'יורד';
+$lang['Enable non-admin powerusers to share albums'] = 'אפשר למשתמשים ללא הרשאות מנהל לשתף אלבומים';
+$lang['For this, add power users into the group <b>\'sharealbum_powerusers\'</b>.'] = 'לכך, הוסף משתמשי כוח אל תוך הקבוצה <b>\'sharealbum_powerusers\'</b>.';
+$lang['Hide menus for albums visitors'] = 'הסתר תפריטים למבקרי אלבומים';
+$lang['IP address'] = 'כתובת IP';
+$lang['Last visit'] = 'ביקור אחרון';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = 'הקישור הועתק בהצלחה ללוח. כעת ניתן להשתמש בפונקציית ההדבקה של המערכת כדי לשתף אותו!';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = 'הקישור נמחק בהצלחה. האלבום אינו משותף בצורה ציבורית יותר';
+$lang['Link was successfully renewed. Click the share button to display it.'] = 'הקישור חודש בהצלחה. לחץ על כפתור השיתוף כדי להציג אותו';
+$lang['No private album to be shared (or which is not yet shared)'] = 'אין אלבום פרטי לשיתוף (או שעדיין לא שותף)';
+$lang['No shared album'] = 'אין אלבום משותף';
+$lang['Number of displayed images per page for shared albums'] = 'מספר התמונות המוצגות לכל עמוד עבור אלבומים משותפים';
+$lang['Please choose an action : '] = 'אנא בחר פעולה:';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = 'אנא בחר את הקישור והשתמש בפונקציית העריכה > העתק של הדפדפן שלך';
+$lang['Renew link'] = 'חידוש קישור';
+$lang['Replace navigation breadcrumbs with album name'] = 'החלף פרורי ניווט בשם האלבום';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = 'מגדיר עוגיית זכרון עבור אורחים מחוברים (כניסה אוטומטית)';
+$lang['Share'] = 'שיתוף';
+$lang['Share a new album'] = 'שיתוף אלבום חדש';
+$lang['Share link was successfully created. Click the share button to display it.'] = 'קישור השיתוף נוצר בהצלחה. לחץ על כפתור השיתוף כדי להציג אותו';
+$lang['Share this album'] = 'שיתוף אלבום זה';
+$lang['Shared by'] = 'משותף על ידי';
+$lang['Shared link'] = 'קישור משותף';
+$lang['Show a login menu'] = 'הצג תפריט התחברות';
+$lang['Show links'] = 'הצג קישורים';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = 'הקישור לא הועתק ללוח. ייתכן שהדפדפן שלך לא תומך בפונקציונליות זו';
+$lang['This album is shared via a public link'] = 'אלבום זה משותף דרך קישור ציבורי';
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = 'פעולה זו תנתק אותך מהאלבום הנוכחי ותטען את עמוד ההתחברות. האם אתה בטוח?';
+$lang['Visit date'] = 'תאריך ביקור';
+$lang['Warning : those users will be able to create shared albums as well as to remove existing shares'] = 'אזהרה: משתמשים אלו יוכלו ליצור אלבומים משותפים וגם להסיר שיתופים קיימים';
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = 'כאשר מסומן, תפריט התחברות מוצג לאורחים שגולשים דרך קישור משותף';
+$lang['When checked, any user belonging to the group \'sharealbum_powerusers\' has access to the share option in private album pages'] = 'כאשר מסומן, כל משתמש השייך לקבוצה \'sharealbum_powerusers\' יכול לגשת לאפשרות השיתוף בעמודי אלבומים פרטיים';
+$lang['When checked, breadcrumbs are replaced with the album name'] = 'כאשר מסומן, פרורי ניווט מוחלפים בשם האלבום';
+$lang['When checked, menus are hidden for visitors of the shared album'] = 'כאשר מסומן, תפריטים מוסתרים למבקרי האלבום המשותף';
+$lang['When checked, nested albums are shared as well as the parent shared album'] = 'כאשר מסומן, אלבומים מקוננים יכולים להיות גלויים כמו כן';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = 'כאשר מסומן, משתמשים יכולים לחזור לכתובת ה-URL הראשית של Piwigo, מחוברים אוטומטית לדפדפן באלבום המשותף האחרון שנצפה בו';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = 'אתה הולך לחדש את הקישור המשותף לאלבום זה. הקישור שנמסר קודם לא יהיה פעיל יותר. האם אתה בטוח?';
