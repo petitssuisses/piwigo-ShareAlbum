8fc107a80a30cfcb3d48cbe70cd92954bdecad5b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-06 05:44:58 +0100

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = '勾選此選項時，將為透過共享連結瀏覽的訪客顯示登入選單';
+$lang['When checked, any user belonging to the group \'sharealbum_powerusers\' has access to the share option in private album pages'] = '勾選此選項後，任何隸屬於 \'sharealbum_powerusers\' 群組的使用者，皆可於私人相簿頁面中使用分享功能';
+$lang['When checked, breadcrumbs are replaced with the album name'] = '勾選此選項後，會使用相簿名稱取代頁面路徑';
+$lang['When checked, menus are hidden for visitors of the shared album'] = '勾選此選項後，共享相簿的訪客就不會看到選單';
+$lang['When checked, nested albums are shared as well as the parent shared album'] = '勾選此選項後，子相簿也能被訪客瀏覽';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = '勾選此選項後，使用者可返回 Piwigo 的根網址，並自動登入瀏覽上次造訪的共享相簿';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = '您即將重新生成此相簿的共享連結。先前發送的連結將不再有效。請確認是否繼續？';
+$lang['No shared album'] = '沒有共享相簿';
+$lang['Number of displayed images per page for shared albums'] = '共享相簿中每頁要顯示的圖片數量';
+$lang['Please choose an action : '] = '請選擇要做的事：';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = '請選取連結，然後使用瀏覽器的「編輯 > 複製」功能。';
+$lang['Renew link'] = '更新連結';
+$lang['Replace navigation breadcrumbs with album name'] = '使用相簿名稱取代頁面路徑';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = '針對自動登入的訪客，使用「記住我」的 cookie';
+$lang['Share'] = '分享';
+$lang['Share a new album'] = '分享新相簿';
+$lang['Share link was successfully created. Click the share button to display it.'] = '已成功產生共享網址。點選「分享」按鈕便可顯示。';
+$lang['Share this album'] = '分享此相簿';
+$lang['Shared by'] = '分享人';
+$lang['Shared link'] = '已分享的網址';
+$lang['Show a login menu'] = '顯示登入選單';
+$lang['Show links'] = '顯示連結網址';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = '無法複製連結網址至剪貼簿。您的瀏覽器可能不支援此功能。';
+$lang['This album is shared via a public link'] = '已透過公開網址共享此相簿';
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = '這麼做會斷開目前相簿的連結，重新載入登入頁面。您確定嗎？';
+$lang['Visit date'] = '造訪日期';
+$lang['Warning : those users will be able to create shared albums as well as to remove existing shares'] = '警告：這些使用者將能建立共享相簿，也能移除現有的共享';
+$lang['Active shares'] = '已共享的相簿';
+$lang['Activity logs shown below'] = '活動記錄如下';
+$lang['Advanced options'] = '進階選項';
+$lang['Apply shares to sub-albums'] = '將分享狀態套用到子相簿中';
+$lang['Are you sure you wish to cancel this album sharing ?'] = '您確定要取消分享此相簿嗎？';
+$lang['Ascending'] = '由小至大';
+$lang['Cancel sharing'] = '取消分享';
+$lang['Connection log for selected album'] = '選定相簿的連結記錄';
+$lang['Copy to clipboard'] = '複製到剪貼簿';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = '定義共享相簿每頁顯示的圖片數量上限。所有的共享相簿都會套用此項設定。';
+$lang['Descending'] = '由大至小';
+$lang['Enable non-admin powerusers to share albums'] = '讓不具管理權限的進階使用者也能分享相簿。';
+$lang['For this, add power users into the group <b>\'sharealbum_powerusers\'</b>.'] = '要使用此功能，請將進階使用者加入 <b>\'sharealbum_powerusers\'</b> 群組。';
+$lang['Hide menus for albums visitors'] = '不要讓相簿訪客看到選單';
+$lang['IP address'] = 'IP 位址';
+$lang['Last visit'] = '上次造訪';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = '連結網址已成功複製至剪貼簿。現在您可以使用系統的「貼上」功能分享網址！';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = '成功刪除連結。已不再公開分享此相簿。';
+$lang['Link was successfully renewed. Click the share button to display it.'] = '成功更新連結。要顯示此相簿，請點擊「分享」按鈕。';
+$lang['No private album to be shared (or which is not yet shared)'] = '不會（或尚未）分享私人相簿。';
