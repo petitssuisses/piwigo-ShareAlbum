839d66a8081ea35cab697961133f238286f920db
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-09 08:24:20 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['When checked, a login menu is shown for guests browsing via a shared link'] = '선택 시 공유 링크를 통해 방문한 게스트에게 로그인 메뉴가 표시됩니다';
+$lang['When checked, any user belonging to the group \'sharealbum_powerusers\' has access to the share option in private album pages'] = '선택 시 \'sharealbum_powerusers\' 그룹에 속한 모든 사용자가 비공개 앨범 페이지에서 공유 옵션에 접근할 수 있습니다';
+$lang['When checked, breadcrumbs are replaced with the album name'] = '선택 시 브레드크럼이 앨범 이름으로 대체됩니다';
+$lang['When checked, menus are hidden for visitors of the shared album'] = '선택 시 공유 앨범 방문자에게 메뉴가 숨겨집니다';
+$lang['When checked, nested albums are shared as well as the parent shared album'] = '체크 시 중첩된 앨범도 탐색할 수 있습니다';
+$lang['When checked, users can go back to root Piwigo url and automatically logged in to browse the last visited shared album'] = '체크 시 사용자는 Piwigo의 루트 URL로 돌아가 마지막으로 방문한 공유 앨범을 자동 로그인 상태로 탐색할 수 있습니다';
+$lang['You are going to renew the shared link for this album. Previously communicated link will no more be active. Do you confirm ?'] = '이 앨범의 공유 링크를 갱신하려고 합니다. 이전에 전달된 링크는 더 이상 유효하지 않습니다. 정말로 진행하시겠습니까?';
+$lang['No shared album'] = '공유 앨범 없음';
+$lang['Number of displayed images per page for shared albums'] = '공유 앨범의 페이지당 표시 이미지 수';
+$lang['Please choose an action : '] = '다음 중 원하는 작업을 선택하세요:';
+$lang['Please select the link and use the Edit > Copy function from your browser.'] = '링크를 선택한 후 브라우저의 편집 > 복사 기능을 사용하세요.';
+$lang['Renew link'] = '링크 새로 고침';
+$lang['Replace navigation breadcrumbs with album name'] = '네비게이션 브레드크럼을 앨범 이름으로 대체';
+$lang['Sets remember me cookie for logged in guests (auto-login)'] = '로그인한 게스트에게 \'내 정보 기억하기\' 쿠키 설정 (자동 로그인)';
+$lang['Share'] = '공유';
+$lang['Share a new album'] = '새 앨범 공유';
+$lang['Share link was successfully created. Click the share button to display it.'] = '공유 링크가 성공적으로 생성되었습니다. 공유 버튼을 클릭하여 표시하세요.';
+$lang['Share this album'] = '이 앨범 공유하기';
+$lang['Shared by'] = '공유자';
+$lang['Shared link'] = '공유 링크';
+$lang['Show a login menu'] = '로그인 메뉴 표시';
+$lang['Show links'] = '링크 표시';
+$lang['The link was not copied to clipboard. Your browser may now support this functionnality.'] = '링크가 클립보드에 복사되지 않았습니다. 브라우저에서 해당 기능을 지원하지 않을 수 있습니다.';
+$lang['This album is shared via a public link'] = '이 앨범은 공개 링크로 공유됩니다';
+$lang['This will disconnect you from the current album and load the identification page. Do you confirm ?'] = '현재 앨범에서 로그아웃되고 로그인 페이지로 이동합니다. 정말로 진행하시겠습니까?';
+$lang['Visit date'] = '방문 날짜';
+$lang['Warning : those users will be able to create shared albums as well as to remove existing shares'] = '경고: 해당 사용자는 공유 앨범을 생성하거나 기존 공유를 삭제할 수 있습니다';
+$lang['Active shares'] = '공유 앨범';
+$lang['Activity logs shown below'] = '아래에 표시된 활동 기록';
+$lang['Advanced options'] = '고급 옵션';
+$lang['Apply shares to sub-albums'] = '하위 앨범에 공유 적용';
+$lang['Are you sure you wish to cancel this album sharing ?'] = '이 앨범 공유를 정말 취소하시겠습니까?';
+$lang['Ascending'] = '오름차순';
+$lang['Cancel sharing'] = '공유 취소';
+$lang['Connection log for selected album'] = '선택한 앨범의 접속 기록';
+$lang['Copy to clipboard'] = '클립보드에 복사';
+$lang['Defines the number of maximum images are displayed per page for shared albums. This setting applies to all shared albums'] = '공유 앨범의 페이지당 최대 표시 이미지 수를 정의합니다. 이 설정은 모든 공유 앨범에 적용됩니다.';
+$lang['Descending'] = '내림차순';
+$lang['Enable non-admin powerusers to share albums'] = '관리자가 아닌 파워 유저의 앨범 공유 허용';
+$lang['For this, add power users into the group <b>\'sharealbum_powerusers\'</b>.'] = '이를 위해 파워 유저를 <b>\'sharealbum_powerusers\'</b> 그룹에 추가하세요.';
+$lang['Hide menus for albums visitors'] = '앨범 방문자에게 메뉴 숨기기';
+$lang['IP address'] = 'IP 주소';
+$lang['Last visit'] = '마지막 방문';
+$lang['Link was successfully copied to clipboard. You can now use system paste functionnality to share it !'] = '링크가 클립보드에 성공적으로 복사되었습니다. 시스템의 붙여넣기 기능을 사용하여 공유할 수 있습니다!';
+$lang['Link was successfully deleted. Album is no longer publicly shared.'] = '링크가 성공적으로 삭제되었습니다. 앨범은 더 이상 공개적으로 공유되지 않습니다.';
+$lang['Link was successfully renewed. Click the share button to display it.'] = '링크가 성공적으로 갱신되었습니다. 공유 버튼을 클릭하여 표시하세요.';
+$lang['No private album to be shared (or which is not yet shared)'] = '공유할 비공개 앨범이 없습니다(또는 아직 공유되지 않은 앨범이 없습니다).';
